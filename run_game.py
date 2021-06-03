6290a4c5a948e113479358a7c25514bdad892dd9
couardcourageux <couardcourageux@gmail.com>
2021-06-03 23:38:17 +0200

    suppression, du __init__ à la racine du module sp_motor

--- run_game.py
+++ run_game.py
@@ -1,7 +1,7 @@
 # from sp_motor.game_classes.map import Map
 # from sp_motor.map.create import create_map
 
-from sp_motor.sp_motor.map.create import create_map
+import sp_motor.game_classes
 
 # import json
 
