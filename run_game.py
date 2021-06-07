2ee4136a8caa1c8f328ae2344a3420fce5f22db9
couardcourageux <couardcourageux@gmail.com>
2021-06-07 09:03:04 +0200

    application de la fonction de cout pour les arretes

--- run_game.py
+++ run_game.py
@@ -1,7 +1,8 @@
 # from sp_motor.game_classes.map import Map
 # from sp_motor.map.create import create_map
-
+import json
 from sp_motor.game_classes.map import Map
+from sp_motor.map.create import print_graph, create_map
 import sp_motor.game_classes
 
 # import json
@@ -11,3 +12,26 @@ import sp_motor.game_classes
 # dico = map.export_info()
 
 # print(dico)
+
+
+
+def test_map():
+
+    def big_map():
+        return create_map(radius=350,nb_zonnes=(70, 90), zonnes_r=(45, 70), systems=(8, 15), inner_conf=(10, 15) )
+
+    def medium_map():
+        return create_map(radius=250,nb_zonnes=(15, 20), zonnes_r=(30, 50), systems=(6, 12), inner_conf=(7, 12) )
+
+
+    map = medium_map()
+    print_graph(map)
+    dico = map.export_info()
+
+
+
+    with open("games/medium_map.json", 'w') as f:
+        json.dump(dico, f)
+
+
+test_map()
--- sp_motor/sp_motor/utils.py
+++ sp_motor/sp_motor/utils.py
@@ -1,10 +1,15 @@
 import math
-
+import numpy as np
 
 def dist(pos1, pos2):
     return math.sqrt((pos1[0] - pos2[0])**2 + (pos1[1] - pos2[1])**2)
 
 
 
-def calculate_cost(dist):
-    return int(dist)
+def calculate_cost(graph):
+    mini_val, max_val = graph.min(), graph.max()
+    for i in range(graph.shape[0]):
+        for j in range(graph.shape[1]):
+            graph[i, j] = int(1+ (graph[i, j] - mini_val)/(max_val - mini_val) * 6  )
+
+    return graph
