94cbedff16bf940c95d5af2510971c7be56d9582
couardcourageux <couardcourageux@gmail.com>
2021-06-08 12:59:15 +0200

    systeme d'authentification

--- web_server/views.py
+++ web_server/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template, redirect, url_for, session
+from flask import Flask, jsonify, request, render_template, redirect, url_for, session, flash
 from functools import wraps
 
 import time
@@ -7,22 +7,27 @@ from datetime import datetime, timedelta
 
 
 from bdd import give_connection
-
+from bdd import add_user, get_user_id, compare_passw
 
 app = Flask(__name__)
 app.static_folder = "../web_docs/data"
 app.template_folder = "../web_docs/templates"
 app.config.from_object('config')
 
+
+bdd_con = 'dev'
+
 ############################
 def login_required(f):
     @wraps(f)
     def wrap(*args, **kwargs):
-        if True:
+        if 'connected' in session and session['connected']:
+            print(session["user_id"])
             return f(*args, **kwargs)
 
         else:
-            print("on redirige vers l'accueil")
+            flash('vous devez etre connecté')
+            return redirect(url_for('connexion'))
     
     return wrap
 
@@ -30,8 +35,49 @@ def login_required(f):
 
 
 @app.route('/')
+@login_required
 def main():
+    session['connected'] = False
     return render_template('indexPyt.html', reload = time.time())
+
+
+@app.route('/connexion', methods=['GET','POST'])
+def connexion():
+    
+    if request.method == 'POST':
+        pseudo = request.form.get('pseudo')
+        passwd = request.form.get('password')
+        if pseudo == None or passwd == None:
+            flash("veuillez remplir le formulaire")
+            return render_template('connexion_pyt.html')
+
+        user_id = get_user_id(pseudo, bdd_con)
+        if user_id == None:
+            add_user(pseudo, passwd, bdd_con)
+            session['connected'] = True
+            session["user_id"] = get_user_id(pseudo, bdd_con)
+            flash('ok !')
+            return redirect(url_for('main'))
+
+        if compare_passw(pseudo, passwd, bdd_con):
+            session['connected'] = True
+            session["user_id"] = get_user_id(pseudo, bdd_con)
+            flash('connexion !')
+            return redirect(url_for('main'))
+
+        flash('mauvais couple id/password')
+        return render_template('connexion_pyt.html')
+
+
+
+        
+
+    else:
+        return render_template('connexion_pyt.html')
+
+
+
+   
 
 
 @app.route('/api/user', methods=['POST'])
@@ -56,7 +102,7 @@ def api_user():
 @app.route('/api/rendu', methods=['POST'])
 def rendu():
     a = int(request.form.get('a'))
-    print(a)
+    
     return jsonify({'resp':a})
 
 @app.route("/connexion", methods=["GET", "POST"])
@@ -69,3 +115,4 @@ def mapSend():
     with open("../game_data/map.json") as f:
         map=json.load(f)
     return jsonify(map)
+    pass
