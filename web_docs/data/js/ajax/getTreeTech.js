6a3c4b259a39f0eabb8e1e26c0e80b312ebe21de
thib_info <thibault.b45@gmail.com>
2021-06-08 13:31:25 +0200

    First implementation of treeTech

--- web_docs/data/js/ajax/getTreeTech.js
+++ web_docs/data/js/ajax/getTreeTech.js
@@ -1,3 +1,6 @@
+let markupArray = ["<ul>"];
+
+getTreeTech();
 
 function getTreeTech(){
     let valueToSend = "";
@@ -6,9 +9,47 @@ function getTreeTech(){
     ajaxCall(valueToSend, target, goal);
 }
 
-function createHTMLCodeTreeTech(){
+// evaluate expressions
+const createList = (items) => {
+    switch (typeof items) {
+        case "object":
+            getItems(items);
+            break;
+    }
+};
+
+// get items in the object
+const getItems = (items) => {
+    for (const item in items) {
+        //markupArray.push(`<li> ${item}`);
+        let details = items[item];
+        getDetails(details);
+        markupArray.push("</li>");
+    }
+};
+
+const getDetails = (details) => {
+    // iterate over the detail items of object
+    let id = details['name'].replace(/ /g, '_');
+    if(details['researched'] === true)
+        markupArray.push(`<li id=${id} class="researched"><span class="tf-nc"> ${details['name']} </span>`);
+    else
+        markupArray.push(`<li id=${id} class="no-researched"><span class="tf-nc"> ${details['name']} </span>`);
+
+    if(Object.entries(details['children']).length !== 0){
+        markupArray.push("</div><ul>");
+        getItems(details['children'][0]);
+        markupArray.push("</ul>");
+    }
+};
+
+function drawTreeTech(jsonTreeTech){
+    createList(jsonTreeTech);
+    markupArray.push("</ul>");
+    let htmlCode = markupArray.join("");
     return null;
 }
+
 
 function askUpdateTech(idTech){
     let valueToSend = "idTech=" + idTech;
