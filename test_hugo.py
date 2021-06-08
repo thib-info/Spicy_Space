b37ed121fd8d7a845a03e331ba1de612d7236081
Flagerr <hugo?baurdy2@gmail.com>
2021-06-08 17:38:39 +0200

    mise a jour de techno qui marche

---
+++ test_hugo.py
@@ -0,0 +1,28 @@
+from sp_motor.game_classes.game import game, save_game, load_game
+from sp_motor.game_classes.player import player
+from sp_motor.game_classes.unit import unit
+from sp_motor.game_classes.building import building as build
+from sp_motor.game_classes.technology import technology
+from sp_motor.utils import load_conf_f
+from copy import deepcopy
+
+
+config = load_conf_f("base_tech")
+
+# print(config["mine"].keys())
+test = technology(config, "usine niveau 2")
+print(test.upgrade)
+print("researched")
+print(test.researched())
+print("upgrade")
+print(test.research())
+print("upgradable")
+print(test.upgradable())
+print("upgrade possible")
+print(test.upgrade_possible())
+print("tech_researched")
+result = test.tech_researched(config)
+print(result)
+print("tech_researchable")
+result2 = test.tech_researchable(config)
+print(result2)
