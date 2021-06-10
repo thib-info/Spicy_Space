936257a49c521787f7887ed5f9cd292ecc7a1963
thib_info <thibault.b45@gmail.com>
2021-06-10 17:08:10 +0200

    end ressource get

--- web_docs/data/js/ajax/getRessources.js
+++ web_docs/data/js/ajax/getRessources.js
@@ -10,5 +10,18 @@ function callRessources(){
 function getRessources(jsonRes){
     let ressources = jsonRes['ressources'];
     console.log(ressources);
-    return null;
+    for (let ressource in ressources){
+        setRessource(ressources[ressource], ressource);
+    }
+}
+
+function setRessource(ressource, name){
+    let currentQt = ressource['qt'];
+    let nextQt = ressource['qt_t'];
+
+    let currentQtPrint = document.getElementById(name + '_q');
+    let nextQtPrint = document.getElementById(name + '_qt');
+
+    currentQtPrint.innerText = currentQt;
+    nextQtPrint.innerText = "+" + nextQt;
 }
